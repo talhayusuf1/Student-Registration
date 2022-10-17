f58d4b2ebe8c21abe174cec7e8cae5bbe7489f6f
Talha Yusuf Aksoy <yusufaksoy0142@gmail.com>
2022-10-17 20:29:58 +0300

    Student-Registration
    This program is purely for educational purposes, student registration is done

---
+++ studentRegistration.py
@@ -0,0 +1,61 @@
+def not_hesapla(satir):
+    satir = satir[:-1]
+    liste = satir.split(":")
+    ogrenciAdi = liste[0]
+    notlar = liste[1].split(",")
+    not1 = int(notlar[0])
+    not2 = int(notlar[1])
+    not3 = int(notlar[2])
+
+    ortalama = (not1+not2+not3)/3
+
+    if ortalama >= 90 and ortalama <= 100:
+        harf = "AA"
+    elif ortalama >= 85 and ortalama <= 89:
+        harf = "BA"
+    elif ortalama >= 65:
+        harf = "CC"
+    else:
+        harf = "FF"
+    return ogrenciAdi + ": "+harf+"\n"
+
+
+def ortalamalari_oku():
+    with open("sinav_notlari.txt", "r", encoding="utf-8") as file:
+        for satir in file:
+            print(not_hesapla(satir))
+
+
+def not_girme():
+    ad = input("Ogrenci adi: ")
+    soyad = input("Ogrenci Soyadi: ")
+    not1 = input("Ogrenci not1: ")
+    not2 = input("Ogrenci not2: ")
+    not3 = input("Ogrenci not3: ")
+
+    with open("sinav_notlari.txt", "a", encoding="utf-8") as file:
+        file.write(ad + " " + soyad+":"+not1+","+not2+","+not3+"\n")
+
+
+def notlari_kayitet():
+    with open("sinav_notlari.txt", "r", encoding="utf-8") as file:
+        liste = []
+
+        for i in file:
+            liste.append(not_hesapla(i))
+        with open("sonuclar.txt", "w", encoding="utf-8") as file2:
+            for i in liste:
+                file2.write(i)
+
+
+while True:
+    islem = input("1- Notları Oku\n2-Not Gir\n3-Notları kayıt Et\n4-Cıkıs\n")
+
+    if islem == "1":
+        ortalamalari_oku()
+    elif islem == "2":
+        not_girme()
+    elif islem == "3":
+        notlari_kayitet()
+    else:
+        break
